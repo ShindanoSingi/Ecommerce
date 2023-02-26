331824b597932c3976f73614b6262346c7231222
ShindanoSingi <shindanosingi@sekur.com>
2023-02-26 10:52:57 -0500

    added user model

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const dbConnect = require('./config/dbConnect');
 const app = express();
 const dotenv = require('dotenv').config();
 const PORT = process.env.PORT || 4000;
 
+dbConnect()
+
+app.use('/', (req, res) => {
+    res.send("Hello from Server side!");
+})
 app.listen(PORT, () => {
-    console.log(`Server is running oon on port ${PORT}`);
+    console.log(`Server is running oon on port ${PORT}.`);
 })
