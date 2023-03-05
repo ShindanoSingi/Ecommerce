fbdd7fb9e1a646611a7110d27773a1783446bd50
ShindanoSingi <shindanosingi@sekur.com>
2023-03-05 15:16:53 -0500

    completed filter, sort, limit and pagination

--- controller/productController.js
+++ controller/productController.js
@@ -61,11 +61,53 @@ const getaProduct = asyncHandler(async (req, res) => {
 
 const getAllProducts = asyncHandler(async (req, res) => {
     try {
-        const getAllProducts = await Product.find();
+        // Filtering
+        const queryObj = { ...req.query };
+        const excludeFields = ['page', 'sort', 'limit', 'fields'];
+        excludeFields.forEach(field => delete queryObj[field]);
+
+        let queryStr = JSON.stringify(queryObj);
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
+
+        let query = Product.find(JSON.parse(queryStr));
+
+        // Sorting
+        if (req.query.sort) {
+            const sortBy = req.query.sort.split(',').join(' ');
+            query = query.sort(sortBy)
+        } else {
+            query = query.sort('-createdAt')
+        }
+        // Limiting the fields
+        if (req.query.fields) {
+            const fields = req.query.fields.split(',').join(' ');
+            query = query.select(fields);
+        } else {
+            query = query.select('-__v');
+        }
+
+        // Pagination
+        const page = req.query.page;
+        const limit = req.query.limit;
+        const skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);
+
+        if (req.query.page) {
+            const productCount = await Product.countDocuments();
+            if (skip >= productCount) throw new Error(`This Page does not contain a product!`)
+        }
+
+        console.log(page, limit, skip);
+
+        const getAllProducts = await Product.find({});
         if (!getAllProducts) {
             res.json({ message: `The are no products!` })
         }
-        res.json(getAllProducts);
+        const product = await query;
+        res.json({
+            sum: product.length, product: product
+        });
     } catch (error) {
         throw new Error(error);
     }
